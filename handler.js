c2ff227016cb69202b1eb8135bad48e178e1a33d
Morgan Gallant <morgan@morgangallant.com>
2021-08-16 01:27:27 -0400

    naughty

--- handler.js
+++ handler.js
@@ -3,6 +3,9 @@ const handler = async (req) => {
     if (req.message == "/start") {
         operand.respond("Welcome! You can say anything, and I'll return it as a caption to a cute cat picture :)");
         return;
+    } else if (req.message.contains("f.inc")) {
+        operand.respond("Mixing business and pleasure huh? Naughty.");
+        return;
     }
     operand.respond(`https://cataas.com/cat/says/${encodeURIComponent(req.message)}`);
 }
