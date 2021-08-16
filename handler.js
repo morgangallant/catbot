23a66aa5d1dbd39054c2f5788a94c3ead12909ee
Morgan Gallant <morgan@morgangallant.com>
2021-08-16 01:26:30 -0400

    back to norma

--- handler.js
+++ handler.js
@@ -3,9 +3,6 @@ const handler = async (req) => {
     if (req.message == "/start") {
         operand.respond("Welcome! You can say anything, and I'll return it as a caption to a cute cat picture :)");
         return;
-    } else if (req.message.contains("f.inc")) {
-        operand.respond("We don't mix business and pleasure.")
-        return;
     }
     operand.respond(`https://cataas.com/cat/says/${encodeURIComponent(req.message)}`);
 }
