c58e1ad5d133068512bd45b45191aec7bcb91de3
Morgan Gallant <morgan@morgangallant.com>
2021-08-16 00:54:57 -0400

    add /start message

--- handler.js
+++ handler.js
@@ -1,4 +1,8 @@
 // A handler which returns a picture of a cat saying a message.
 const handler = async (req) => {
+    if (req.message == "/start") {
+        operand.respond("Welcome! You can say anything, and I'll return it as a caption to a cute cat picture :)");
+        return;
+    }
     operand.respond(`https://cataas.com/cat/says/${encodeURIComponent(req.message)}`);
 }
