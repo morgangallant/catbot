a3c033d1ce6bfb73eb3c01d5a98dcded9cc9a1bd
Morgan Gallant <morgan@morgangallant.com>
2021-08-16 17:03:04 -0400

    adding logging

--- handler.js
+++ handler.js
@@ -1,5 +1,6 @@
 // A handler which returns a picture of a cat saying a message.
 const handler = async (req) => {
+    operand.log(`Got request ${req.message}!`);
     if (req.message == "/start") {
         operand.respond("Welcome! You can say anything, and I'll return it as a caption to a cute cat picture :)");
         return;
