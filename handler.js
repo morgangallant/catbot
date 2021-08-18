3ba1ef907e8e97f122b8bd66cd06a7e1747f8d6d
Morgan Gallant <morgan@morgangallant.com>
2021-08-18 13:38:36 -0400

    jason demo

--- handler.js
+++ handler.js
@@ -3,6 +3,9 @@ const handler = async (req) => {
     if (req.message == "/start") {
         operand.respond("Welcome! You can say anything, and I'll return it as a caption to a cute cat picture :)");
         return;
+    } else if (req.message == "jason") {
+        operand.respond("yes, jason is cool");
+        return;
     }
     operand.respond(`https://cataas.com/cat/says/${encodeURIComponent(req.message)}`);
     operand.log("Handled incoming cat request.");
